f5b6fc998f8ccef9077443e6c050e9a5e804b349
Pranjul Gupta <pranjul.here@gmail.com>
2025-06-05 11:24:50 +0530

    BasicCalculator.c

--- C-Lang/BasicCalculator.c
+++ C-Lang/BasicCalculator.c
@@ -14,3 +14,24 @@ int main()
     printf("DIVISION: %d / %d = %.2f\n",a,b,(float) a/b);
     printf("REMAINDER: when %d / %d REMAINDER = %d\n",a,b,a%b);
 }
+
+//BELOW IS THE SAME CODE WRAPPED IN A FUNCTION.
+
+// #include <stdio.h>
+
+// void calc()
+// {
+//     int a,b;
+//     printf("Enter the number1 followed by number 2 seperated by \'space\' : ");
+//     scanf("%d %d", &a, &b);
+//     printf("SUM: %d + %d = %d\n",a,b,a+b);
+//     printf("DIFFERENCE: %d - %d = %d\n",a,b,a-b);
+//     printf("PRODUCT: %d X %d = %d\n",a,b,a*b);
+//     printf("DIVISION: %d / %d = %.2f\n",a,b,(float) a/b);
+//     printf("REMAINDER: when %d / %d REMAINDER = %d\n",a,b,a%b);
+// }
+
+// int main(){
+//     calc();
+//     return 0;
+// }
