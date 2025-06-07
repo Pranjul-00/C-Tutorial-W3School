04eee995eded77d005f23796840cb8213e2653ac
Pranjul Gupta <pranjul.here@gmail.com>
2025-06-07 09:54:02 +0530

    Add files via upload

---
+++ C-Lang/Lesson-ENUM.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+enum level
+{
+    LOW=1,
+    MEDIUM,
+    HIGH
+};
+
+int main()
+{
+    enum level x=HIGH;
+
+    switch (x)
+    {
+    case 1:
+        printf("aji lun mera");
+        break;
+
+    case 2:
+        printf("sorry guys");
+        break;
+
+    case 3:
+        printf("DUCKKKKKKKKKKKKKKKKKKKKK");
+        break;
+    
+    default:
+        printf("Damn idk nigga");
+        break;
+    }
+    return 0;
+}
